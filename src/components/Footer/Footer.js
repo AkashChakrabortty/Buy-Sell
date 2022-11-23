01792043dd3033e23036bfe114c5517961b6b76a
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-11-23 23:08:09 +0600

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            footer
+  return (
+    <div className="container">
+      <footer>
+        <div className="">
+          <div className="text-center">
+            <span>© 2022 Company, Inc. All rights reserved.</span>
+          </div>
         </div>
-    );
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
